d41be24330a9e1f798efacdd8beb5b3e0552cb2b
ajacobson247 <ajacobson247@gmail.com>
2025-12-29 11:31:36 -0600

    updated supabase env variable keys

--- new-years-2026/lib/supabase/anon-server.ts
+++ new-years-2026/lib/supabase/anon-server.ts
@@ -3,8 +3,8 @@ import "server-only";
 import { createClient } from "@supabase/supabase-js";
 
 export function getSupabaseAnonServerClient() {
-  const url = process.env.SUPABASE_URL;
-  const anonKey = process.env.SUPABASE_ANON_KEY;
+  const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const anonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
 
   if (!url || !anonKey) {
     throw new Error("Missing SUPABASE_URL or SUPABASE_ANON_KEY env vars");
